e4df0d254f0e2372d7b5d7d3b0795aa005d5171e
guan3982857 <519844708@qq.com>
2016-12-26 18:59:50 +0800

    1
    1

---
+++ js/index.js
@@ -0,0 +1,24 @@
+  var mySwiper = new Swiper ('.swiper-container', {
+    direction: 'vertical',
+    loop: true,
+    autoplay : 3000,
+    autoplayDisableOnInteraction : false,
+    // 如果需要分页器
+    pagination: '.swiper-pagination'
+    
+    // 如果需要前进后退按钮
+    // nextButton: '.swiper-button-next',
+    // prevButton: '.swiper-button-prev',
+    
+    // // 如果需要滚动条
+    // scrollbar: '.swiper-scrollbar',
+  })   
+  ._1 {
+  
+  position: absolute;
+  left: 19px;
+  top: 441px;
+  width: 157px;
+  height: 156px;
+  z-index: 34;
+}
